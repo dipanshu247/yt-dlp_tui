a49cea4091b9b8d46740bc4fc40d3f52417e673f
Dipanshu Kumar <dipanshu247kumar@gmail.com>
2025-05-06 21:54:54 +0530

    fixed a issue

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mkdir ~/.aspr
+cd ~/.aspr
 curl -L -o run.sh https://github.com/dipanshu247/my-linux-themes/releases/download/first-testing/run.sh
 echo "alias ytd="~/.aspr/run.sh"" >> ~/.bashrc
