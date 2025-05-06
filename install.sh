6b72b6d8679c4884dbbad75d8ac869c0284c1c42
dipanshu247 <dipanshu247kumar@gmail.com>
2025-05-06 22:01:40 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 mkdir ~/.aspr
 cd ~/.aspr
 curl -L -o run.sh https://github.com/dipanshu247/my-linux-themes/releases/download/first-testing/run.sh
-chmod +x run.sh
+chmod +x ~/.aspr/run.sh
 echo "alias ytd="~/.aspr/run.sh"" >> ~/.bashrc
