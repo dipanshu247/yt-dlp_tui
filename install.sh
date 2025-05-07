4f3ea05ef4bef135bf4621a23011e60978f7005d
dipanshu247 <dipanshu247kumar@gmail.com>
2025-05-07 12:33:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ cd ~/.aspr
 curl -L -o run.sh https://github.com/dipanshu247/my-linux-themes/releases/download/first-testing/run.sh
 chmod +x ~/.aspr/run.sh
 echo "alias ytd="~/.aspr/run.sh"" >> ~/.bashrc
+source ~/.bashrc
+clear
+echo "Installation finished Successfully"
